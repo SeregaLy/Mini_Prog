3ea01e53ce9c20730e56b7fb0ac53c0ba31f83f3
SeregaLi <kaprall13rus@gmail.com>
2023-01-30 10:26:46 +0300

    1-3

--- main.py
+++ main.py
@@ -18,10 +18,28 @@ def speed_test():
     label_1.place(x=150, y=10)
 
     label_2 = tkinter.Label(main_window, text='Start Typing', font='times 20')
-    label_2.place(x=10 , y = 50)
+    label_2.place(x=10, y=50)
 
     entry = tkinter.Entry(main_window)
-    entry.place(x=280, y = 55)
+    entry.place(x=280, y=55)
+
+    def check_result():
+        if entry.get() == sentence:
+            end = timer()
+            label_3.configure(text=f'Time: {round((end - start), 4)}s')
+        else:
+            label_3.configure(text='Wrong Input')
+
+    button_1 = tkinter.Button(main_window, text='Done', command=check_result(),
+                              width=12, bg='grey')
+    button_1.place(x=150, y=100)
+    button_2 = tkinter.Button(main_window, text='Try Again',
+                              command=speed_test, width=12, bg='grey')
+    button_2.place(x=10, y=300)
+
+    label_3 = tkinter.Label(main_window, text='', font='times 20')
+    label_3.place(x=10, t=300)
+    main_window.mainloop()
 
 
 if __name__ == '__main__':
