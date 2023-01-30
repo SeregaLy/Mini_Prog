368d2dbbd52f28a964dc94082e3d5ed6937b06a4
SeregaLi <kaprall13rus@gmail.com>
2023-01-30 10:15:39 +0300

    1-2

--- main.py
+++ main.py
@@ -14,5 +14,15 @@ def speed_test():
     main_window = tkinter.Tk()
     main_window.geometry('600x400')
 
+    label_1 = tkinter.Label(main_window, text=sentence, font='times 20')
+    label_1.place(x=150, y=10)
+
+    label_2 = tkinter.Label(main_window, text='Start Typing', font='times 20')
+    label_2.place(x=10 , y = 50)
+
+    entry = tkinter.Entry(main_window)
+    entry.place(x=280, y = 55)
+
+
 if __name__ == '__main__':
     speed_test()
